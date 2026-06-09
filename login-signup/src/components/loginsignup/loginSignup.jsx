f4eb7859d2c61a1b9e7a0af96e495e46e0ae8ccd
TheePoseidon <j.ngugigiat@alustudent.com>
2026-06-09 16:48:51 +0200

    Assets

---
+++ login-signup/src/components/loginsignup/loginSignup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './loginSignup.css'
+import user_icon from '../../assets/mx-logo.png'
+import email_icon from '../../assets/mx-logo.png'
+import password_icon from '../../assets/mx-logo.png'
+
+export const loginSignup = () => {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default loginSignup
